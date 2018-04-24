07a0a5666ef1f510304091cbb3d958f26d0339f3
sandeep <30605278+Sandeep501@users.noreply.github.com>
2018-04-24 21:39:39 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ function clikme() {
     var x = document.getElementById('para');
     document.getElementById('para').innerHTML = " ";
     var date = new Date();
-    return [date.getHours(), date.getMinutes(), date.getSeconds()].map(function (time) {
+    var hours=date.getHours();
+    return [hours%12,':',date.getMinutes(),':', date.getSeconds()].map(function (time) {
 
         x.innerHTML += time;
-        //console.log(date.getHours(),date.getMinutes(),date.getSeconds());
+       
         document.getElementById('big-body').style.backgroundColor = 'rgb(' + [date.getHours(), date.getMinutes(), date.getSeconds()].join(',') + ')';
 
-        // body...
     });
 }
