ef91451bc65502820a8bb620b98e436af0b0e6f1
sandeep <30605278+Sandeep501@users.noreply.github.com>
2018-04-24 22:12:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ function clikme() {
     var x = document.getElementById('para');
     document.getElementById('para').innerHTML = " ";
     var date = new Date();
-    var hours=date.getHours();
-    return [hours%12,':',date.getMinutes(),':', date.getSeconds()].map(function (time) {
+    var hours=(date.getHours()%12);
+    var minutes=date.getMinutes();
+    var sec=date.getSeconds();
+
+    if(hours<=9)
+    {
+        hours='0'+hours;
+        
+      
+    }
+    if( minutes<=9)
+    {
+        minutes='0'+minutes;
+
+    }
+    if(sec<=9){
+        sec='0'+sec;
+    }
+
+    return [hours,':',minutes,':', sec].map(function (time) {
 
         x.innerHTML += time;
        
